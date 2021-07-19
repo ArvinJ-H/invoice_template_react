c566f351b9e2bc0a1b81b79ae4565485ac32df58
Arvin Han <arvinhjt0718@gmail.com>
2021-07-19 15:36:35 +1000

    Change payment info to editable

--- src/component/paymentInfo.jsx
+++ src/component/paymentInfo.jsx
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useRef } from "react";
 
-function paymentInfo() {
-  return (
-    <div className="border">
-      <p className="name">Payment Info</p>
-      <br />
-      <p className="otherDetail">BSB: 062181</p>
-      <p className="otherDetail">Account No. :10704326</p>
-      <br />
-      <p className='name'>TOTAL DUE</p>
-      <p className='money'> AU$ 10,000</p>
-    </div>
-  );
+import ContentEditable from "react-contenteditable";
+
+function PaymentInfo() {
+    const bsb = useRef("062181");
+    const accountNo = useRef("10704326");
+
+    const handleBSB = (evt) => {
+        bsb.current = evt.target.value;
+    };
+    const handleAccountNo = (evt) => {
+        accountNo.current = evt.target.value;
+    };
+    const handleBlur = () => {
+        console.log(bsb.current);
+        console.log(accountNo.current);
+    };
+    return (
+        <div className="border">
+            <p className="name">Payment Info</p>
+            <br />
+            <div className="invoice-payment">
+                <p className="otherDetail">BSB:&nbsp;</p>
+                <ContentEditable
+                    className="paymentDetail"
+                    html={bsb.current}
+                    onBlur={handleBlur}
+                    onChange={handleBSB}
+                />
+            </div>
+            <div className="invoice-payment">
+                <p className="otherDetail">Account No. :&nbsp;</p>
+                <ContentEditable
+                    className="paymentDetail"
+                    html={accountNo.current}
+                    onBlur={handleBlur}
+                    onChange={handleAccountNo}
+                />
+            </div>
+
+            <p className="name">TOTAL DUE</p>
+            <p className="money"> AU$ 10,000</p>
+        </div>
+    );
 }
 
-export default paymentInfo;
+export default PaymentInfo;
